98e96050409f91275813b8aef4f6c56da56e4c7c
Denis Yordanov <deyordanov777@gmail.com>
2024-06-23 17:24:50 +0300

    Added tests for the Options domain model.

---
+++ CarRentalSystem.Domain/Models/CarAds/Tests/OptionsTests.cs
@@ -0,0 +1,51 @@
+﻿namespace CarRentalSystem.Domain.Models.CarAds.Tests;
+
+using CarRentalSystem.Domain.Exceptions;
+
+using FluentAssertions;
+
+using Xunit;
+
+using static ModelConstants.Options;
+
+public class OptionsTests
+{
+    private const int ValidOptionsNumberOfSeats = 5;
+    
+    [Fact]
+    public void Options_ShouldNot_ThrowException_WhenCreatedWithValidData()
+    {
+        // Arrange
+        var act = CreateValid;
+        
+        // Act & Assert
+        act.Should().NotThrow<InvalidCarAdException>();
+    }
+
+    [Fact]
+    public void Options_Should_ThrowException_WhenCreatedWithInvalidNumberOfSeats()
+    {
+        // Arrange
+        var invalidNumberOfSeatsUnderMinRange = MinimumNumberOfSeats - 1;
+        var actInvalidUnderMinRange = () => Create(invalidNumberOfSeatsUnderMinRange);
+
+        var invalidNumberOfSeatsOverMaxRange = MaximumNumberOfSeats + 1;
+        var actInvalidOverMaxRange = () => Create(invalidNumberOfSeatsOverMaxRange);
+        
+        // Act & Assert
+        actInvalidUnderMinRange.Should().Throw<InvalidOptionsException>();
+        actInvalidOverMaxRange.Should().Throw<InvalidOptionsException>();
+    }
+
+    private static Options CreateValid()
+        => new Options(
+            true,
+            ValidOptionsNumberOfSeats,
+            TransmissionType.Automatic);
+
+    private static Options Create(int numberOfSeats)
+        => new Options(
+            true,
+            numberOfSeats,
+            TransmissionType.Automatic);
+}
