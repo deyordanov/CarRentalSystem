e43b0c373632c267efc01d7457e1f8891a9e3133
Denis Yordanov <deyordanov777@gmail.com>
2024-06-23 17:45:41 +0300

    Created tests for the Dealer domain model.

---
+++ CarRentalSystem.Domain/Models/Dealers/Tests/DealerTests.cs
@@ -0,0 +1,46 @@
+﻿namespace CarRentalSystem.Domain.Models.Dealers.Tests;
+
+using CarRentalSystem.Domain.Exceptions;
+
+using FluentAssertions;
+
+using Xunit;
+
+using static ModelConstants.Common;
+
+public class DealerTests
+{
+    private const string ValidName = "Valid name";
+    private const string ValidPhoneNumber = "+359894567432";
+    
+    [Fact]
+    public void Dealer_ShouldNot_ThrowException_WhenCreatedWithValidData()
+    {
+        // Arrange
+        var act = CreateValid;
+        
+        // Act & Assert
+        act.Should().NotThrow<InvalidDealerException>();
+    }
+
+    [Fact]
+    public void Dealer_Should_ThrowException_WhenCreatedWithInvalidName()
+    {
+        // Arrange
+        var invalidNameUnderMinLength = new string('*', MinimumNameLength - 1);
+        var actUnderMinLength = () => Create(invalidNameUnderMinLength, ValidPhoneNumber);
+
+        var invalidNameOverMaxLength = new string('*', MaximumNameLength + 1);
+        var actOverMaxLength = () => Create(invalidNameOverMaxLength, ValidPhoneNumber);
+        
+        // Act & Assert
+        actUnderMinLength.Should().Throw<InvalidDealerException>();
+        actOverMaxLength.Should().Throw<InvalidDealerException>();
+    }
+
+    private static Dealer CreateValid()
+        => new Dealer(ValidName, ValidPhoneNumber);
+
+    private static Dealer Create(string name, PhoneNumber phoneNumber)
+        => new Dealer(name, phoneNumber);
+}
