ccb532259bf44677b03193730eba32c1bcbd5d49
Denis Yordanov <deyordanov777@gmail.com>
2024-06-23 16:47:16 +0300

    Created tests for the Manufacturer domain model.

---
+++ CarRentalSystem.Domain/Models/CarAds/Tests/ManufacturerTests.cs
@@ -0,0 +1,45 @@
+﻿namespace CarRentalSystem.Domain.Models.CarAds.Tests;
+
+using CarRentalSystem.Domain.Exceptions;
+
+using FluentAssertions;
+
+using Xunit;
+
+using static ModelConstants.Common;
+
+public class ManufacturerTests
+{
+    private const string ValidManufacturer = "Valid manufacturer";
+
+    [Fact]
+    public void Manufacturer_ShouldNot_ThrowException_WhenCreatedWithValidData()
+    {
+        // Arrange
+        var act = CreateValid;
+        
+        // Act & Assert
+        act.Should().NotThrow<InvalidCarAdException>();
+    }
+
+    [Fact]
+    public void Manufacturer_Should_ThrowException_WhenCreatedWithInvalidName()
+    {
+        // Arrange
+        var invalidNameUnderMinLength = new string('*', MinimumNameLength - 1);
+        var actUnderMinLength = () => Create(invalidNameUnderMinLength);
+
+        var invalidNameOverMaxLength = new string('*', MaximumNameLength + 1);
+        var actOverMaxLength = () => Create(invalidNameOverMaxLength);
+        
+        // Act & Assert
+        actUnderMinLength.Should().Throw<InvalidCarAdException>();
+        actOverMaxLength.Should().Throw<InvalidCarAdException>();
+    }
+    
+    private static Manufacturer CreateValid()
+        => new Manufacturer(ValidManufacturer);
+
+    private static Manufacturer Create(string manufacturer)
+        => new Manufacturer(manufacturer);
+}
