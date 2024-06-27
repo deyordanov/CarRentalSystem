cd31bc9574cf0dc3af1e8a9c8df67ff2ff9dc9be
Denis Yordanov <deyordanov777@gmail.com>
2024-06-27 21:18:05 +0300

    Added a reference to ASP.NET Core framework, as well as a CarAdsController.
    Added a reference to the ASP.NET Core framework ( in order to be able to work with ASP.NET Core's rich set of features and libraries inside the Class Library, such as dependency injection, middleware pipelines, and MVC ).

---
+++ CarRentalSystem.Web/Features/CarAdsController.cs
@@ -0,0 +1,20 @@
+﻿namespace CarRentalSystem.Web.Features;
+
+using CarRentalSystem.Domain.Models.CarAds;
+using CarRentalSystem.Domain.Models.Dealers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class CarAdsController
+{
+    private static readonly Dealer Dealer
+        = new Dealer("Dealer", "+359893742569");
+
+    [HttpGet]
+    public IEnumerable<CarAd> Get()
+        => Dealer
+            .CarAds
+            .Where(c => c.IsAvailable);
+}
